98a1fb1cfa5da15ed0b1823e27c6977d7e8eb713
Timm Hirsens <timmhirsens@gmail.com>
2021-12-12 13:09:52 +0100

    Day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,36 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var previous: Int? = null
+        var increased = 0
+        for (line in input) {
+            val newValue = line.toInt()
+            if (previous != null && newValue > previous) {
+               increased++
+            }
+            previous = newValue
+        }
+        return increased
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val max = input.size - 3
+        var previous: Int? = null
+        val measurements = input.map { it.toInt() }
+        var increased = 0
+        for (i in 0..max) {
+           val newValue = measurements[i] + measurements[i + 1] + measurements[i + 2]
+            if (previous != null && newValue > previous!!) {
+                increased++
+            }
+            previous = newValue
+        }
+
+        return increased
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
 
     val input = readInput("Day01")
     println(part1(input))
